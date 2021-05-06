016a955b0395cd157030a735a73df0308e1aba7c
moamlrh <moamll288@gmail,com>
2021-05-06 21:27:34 +0300

    add all to github

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+import './index.css'
+const App = () => {
+    return (
+        <h1>Hi baby</h1>
+    )
+}
+ReactDom.render(<App/>, document.getElementById('root'))
